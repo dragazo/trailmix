a2b81758ad4755540920893554c8468b70ac3852
Devin Jean <emailcruzjean@yahoo.com>
2026-01-09 09:21:04 -0600

    multi events

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ let LOG_URI = null;
 
 const format_pos = (p: vscode.Position) => `${p.line + 1}:${p.character + 1}`;
 
-function log(kind: string, event: Record<string, string>) {
+function log(kind: string, event: Record<string, string | Array<Record<string, string>>>) {
     const entry = { t: new Date().getTime(), k: kind, ...event };
     fs.appendFileSync(LOG_URI!.fsPath, JSON.stringify(entry) + '\n');
     console.log(entry);
@@ -27,21 +27,17 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(vscode.workspace.onDidCloseTextDocument(e => log('close', { f: e.fileName })));
     context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(e => log('save', { f: e.fileName })));
-    context.subscriptions.push(vscode.workspace.onDidRenameFiles(e => e.files.forEach(e => log('rename', { b: e.oldUri.fsPath, a: e.newUri.fsPath }))));
+    context.subscriptions.push(vscode.workspace.onDidRenameFiles(e => log('rename', { m: e.files.map(x => ({ b: x.oldUri.fsPath, a: x.newUri.fsPath })) })));
 
     context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(e => {
         if (e.textEditor.document.fileName.endsWith(LOG_NAME)) return;
-        for (const x of e.selections) {
-            log('select', { f: e.textEditor.document.fileName, s: format_pos(x.start), e: format_pos(x.end) });
-        }
+        log('select', { f: e.textEditor.document.fileName, m: e.selections.map(x => ({ s: format_pos(x.start), e: format_pos(x.end) })) });
     }));
 
     context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(e => {
         if (e.document.fileName.endsWith(LOG_NAME)) return;
         const k = e.reason ? { [vscode.TextDocumentChangeReason.Undo]: 'undo', [vscode.TextDocumentChangeReason.Redo]: 'redo' }[e.reason] : 'edit';
-        for (const x of e.contentChanges) {
-            log(k, { f: e.document.fileName, s: format_pos(x.range.start), e: format_pos(x.range.end), c: x.text });
-        }
+        log(k, { f: e.document.fileName, m: e.contentChanges.map(x => ({ s: format_pos(x.range.start), e: format_pos(x.range.end), c: x.text })) });
     }));
 
     const watcher = vscode.workspace.createFileSystemWatcher("**/*");
